c0035d9c3652d0a9c595ecf61386d36250094104
Dave Eddy <dave@daveeddy.com>
2012-07-29 20:39:38 -0700

    Initial Commit

---
+++ README.md
@@ -0,0 +1,21 @@
+celery-man
+==========
+
+Computer load up celery man
+
+http://www.youtube.com/watch?v=XWX4GUYGQXQ
+
+Flarhgunnstow
+-------------
+
+![Rudd](http://daveeddy.com/static/media/github/rudd.gif)
+
+Install
+-------
+
+    npm install -g celery-man
+
+License
+-------
+
+MIT Licensed
---
+++ bin/celery-man.js
@@ -0,0 +1,40 @@
+#!/usr/bin/env node
+/**
+ * Engage 4d3d3d3
+ *
+ * http://www.youtube.com/watch?v=XWX4GUYGQXQ
+ *
+ * Author: Dave Eddy <dave@daveeddy.com>
+ */
+
+var fs = require('fs'),
+    path = require('path'),
+    clear = require('clear'),
+    delay = (+process.argv[2]) ? +process.argv[2] : 40, // ms
+    engage_delay = 1 * 500,
+    celery_path = path.join(__dirname, '../celery'),
+    celery = [];
+
+// Load up the celery man
+process.stdout.write('4d3d3d3... ');
+fs.readdirSync(celery_path).forEach(function(file, i) {
+  celery[i] = fs.readFileSync(path.join(celery_path, file), 'ascii');
+});
+console.log('engaged');
+
+// Engage it
+function _4d3d3d3(rudd) {
+  clear(false);
+  console.log(rudd);
+}
+
+// Flarhgunnstow
+var i = 0;
+setTimeout(function() {
+  clear();
+  setInterval(function() {
+    // Hat Wobble
+    _4d3d3d3(celery[i]);
+    i = (i + 1) % celery.length;
+  }, delay);
+}, engage_delay);
